ca0435bd215b3bb51dbef4d7ab2b0e0df4f1572f
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2024-02-27 10:26:39 -0500

    Initializing

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+//----    ----    ----
+
+//styles:
+import "../styles/reset.css";
+import "../styles/styles.css";
+
+//----    ----    ----
+/*
+  Dependencies:
+
+  npm install react
+  npm install parcel-bundler
+
+*/
+//.
