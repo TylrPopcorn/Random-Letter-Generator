ea0f10176e4b926c0a705d387c879b977c87677a
TylrPopcorn <tyler.mcmillan99@yahoo.com>
2024-03-01 10:22:53 -0500

    Working on

---
+++ src/Components/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import ACTIONS, { ACTION_TYPES } from "../state/action-creators"; //helpers
+//----- ------  ------- ---------
+//----Main function:
+class App extends React.Component {
+  //
+  //
+  //
+
+  render() {
+    return <div></div>;
+  }
+}
+
+//Used for reducer:
+const mapstateToProps = (stateFromStore) => {
+  return stateFromStore; //Will import props.
+};
+
+export default connect(mapstateToProps, ACTIONS)(App); //Props / Reducer
